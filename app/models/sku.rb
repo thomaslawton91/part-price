7d89381314547226edea8bb1d72cde22a85977bc
Thomas Lawton <thomaslawton91@gmail.com>
2018-10-01 13:43:04 -0400

    Fixes validation issue

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -1,15 +1,6 @@
 class Sku < ApplicationRecord
   validates :sku_label, presence: true
-  validates :jit_cogs, presence: true
-  validates :domestic_stocking_cogs, presence: true
-  validates :best_stocking_cogs, presence: true
-  validates :genuineeqprice, presence: true
-  validates :reporting_make, presence: true
-  validates :brand_name, presence: true
-  validates :category_name, presence: true
-  validates :sub_category_name, presence: true
-  validates :functional_name, presence: true
-  
+
   def self.import(file)
     Sku.delete_all
     CSV.foreach(file.path, headers: true) do |row|
